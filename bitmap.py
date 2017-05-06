7ad5ad6be1942250b8ca9a0f1647d57b103ba057
Arthur Taylor <arthur.taylor@gmail.com>
2017-05-06 11:10:53 +0000

    Updated bitmap.py to use ledutils from arduino_lights

--- bitmap.py
+++ bitmap.py
@@ -1,5 +1,7 @@
 import serial, Image
 import time, math
+from arduino_lights import ledutils
+
 ser = serial.Serial(
     #port='/dev/ttyACM0',
     port='/dev/ttyUSB0',
@@ -11,42 +13,20 @@ ser = serial.Serial(
 # before running the actual already stored code 
 time.sleep(2)
 
-def set_pixel(pixel, red, green, blue):
-  red   = min(red, 253)
-  green = min(green, 253)
-  blue  = min(blue, 253)
-
-  control_string = bytearray([pixel,red,green,blue, 255])
-  ser.write(control_string)    
-
-def xy_to_pixel(x,y):
-  row_offset = y * 12
-  if y % 2 == 0:
-    column_offset = (11 - x)
-  else:
-    column_offset = x
-  return row_offset + column_offset
-
-
-
 def render_image(image_path):
   img = Image.open("images/" + image_path)
   rgb_im = img.convert('RGB')
 
   sleep_time = 0.002
-  #sleep_time = 0.01
 
-  control_string = bytearray([254])
-  ser.write(control_string)  
-
+  ledutils.end_frame(ser)
 
   print "time per frame: " + str(sleep_time * 144) + "s"
 
   for x in range(12):
     for y in range(12):
       r,g,b = rgb_im.getpixel((x,y))
-      pixel = xy_to_pixel(x,11 - y)
-      set_pixel(pixel,r,g,b)
+      ledutils.set_pixel(ser, x, 11 - y, r, g, b)
       time.sleep(sleep_time)
 
 while(True):
