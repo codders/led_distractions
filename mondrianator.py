b0a727ae8ac22ba4f7599bcfe52f41a1f71c7077
Ezequiel Muns <ezeq.au@gmail.com>
2017-05-30 02:41:48 +1000

    Improvements to mondrianator
    * colored fields can only be one color
    * cycle endlessly through different compositions
    * slower adding of color at the begining
    * cosmetic improvements

--- mondrianator.py
+++ mondrianator.py
@@ -2,12 +2,20 @@
 '''Composition in Red, Blue and Yellow art generator'''
 import time
 import argparse
-from random import choice, randint, random
+from random import choice, randint, random, seed
 import math
 from itertools import product
 import arduino_lights as al
 from arduino_lights import LED_SIZE, LED_COUNT
 
+PROB_LINE = 0.6
+PROB_COLOR = 0.20
+PROB_INTERSECT_ENDS = 0.3
+MIN_LINES = 3
+MAX_LINES = 9
+MIN_PAINTS = 3
+MAX_PAINTS = 6
+DELIGHTMENT_TIME = 10 # seconds
 
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
@@ -15,16 +23,22 @@ BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
 BLACK = (0, 0, 0)
 
-painting = {xy: WHITE
-            for xy in product(xrange(LED_SIZE.w), xrange(LED_SIZE.h))}
 
-lines = set()
-paints = 0
-active_lines = []
-active_paints = []
+def color_compare(color1, color2):
+    '''Produce an ordering of colors so we can settle color-disputes in a field.
+    We'll do this in a non-transitive way, so that no color dominates.'''
+    # BLACK > COLORS > WHITE
+    # COLORS: RED > BLUE, BLUE > YELLOW, YELLOW > RED
+    return {
+        BLACK: 1,
+        RED:    {BLACK: -1, WHITE: 1, RED: 0, BLUE: 1, YELLOW: -1}.get(color2),
+        BLUE:   {BLACK: -1, WHITE: 1, RED: -1, BLUE: 0, YELLOW: 1}.get(color2),
+        YELLOW: {BLACK: -1, WHITE: 1, RED: 1, BLUE: -1, YELLOW: 0}.get(color2),
+        WHITE: -1
+    }.get(color1)
 
 
-def add_line():
+def add_line(lines, active_lines):
     while True:
         line = (choice(['h', 'v']),
                 choice(xrange(LED_SIZE.w)))
@@ -42,26 +56,25 @@ def add_line():
         'current': current,
         'done': False
     })
-    print "Added line %s" % str(active_lines[-1])
+    print "Added line %s" % str(line)
 
 
-def add_paint():
-    global paints
+def add_paint(painting, active_paints):
+    color = choice([RED, BLUE, YELLOW])
     while True:
         xy = (randint(0, LED_SIZE.w - 1), randint(0, LED_SIZE.h - 1))
-        if painting[xy] == WHITE:
+        if color_compare(color, painting[xy]) > 0:
             break
     active_paints.append({
         'origin': xy,
-        'color': choice([RED, BLUE, YELLOW]),
+        'color': color,
         'done': False,
     })
-    paints += 1
-    print "Added paint %s" % str(active_paints[-1])
+    print "Added paint %s" % str((xy, color))
 
 
-def grow_paints():
-    global active_paints
+def grow_paints(painting, active_paints):
+    init = len(active_paints)
     for paint in active_paints:
         if 'perimeter' not in paint:
             paint['perimeter'] = {paint['origin']}
@@ -70,71 +83,95 @@ def grow_paints():
             new_perimeter = set()
             for point in paint['perimeter']:
                 # paint all neighbours of perimeter that are white
+                # or less than our color
                 for direction in product([-1, 0, 1], [-1, 0, 1]):
                     if direction[0] == direction[1] == 0:
                         continue
                     neighbour = (point[0] + direction[0], point[1] + direction[1])
                     if any(d < 0 or d >= LED_SIZE.h for d in neighbour):
                         continue
-                    if painting[neighbour] == WHITE:
+                    if color_compare(paint['color'], painting[neighbour]) > 0:
                         painting[neighbour] = paint['color']
                         new_perimeter.add(neighbour)
             if len(new_perimeter) == 0:
                 paint['done'] = True
             else:
                 paint['perimeter'] = new_perimeter
-    active_paints = [p for p in active_paints if not p['done']]
+
+    active_paints[:] = [p for p in active_paints if not p['done']]  # modify in-place
+    return init - len(active_paints)
 
 
-def grow_lines():
-    global active_lines
+def grow_lines(painting, active_lines):
     def orient(x, y, orientation):
         if orientation == 'v':
             return (x, y)
         else:
             return (y, x)
 
-    W, H = LED_SIZE
+    init = len(active_lines)
     for line in active_lines:
         line['current'] += line['direction']
-        if  line['current'] < 0 or line['current'] >= W:
+        if  line['current'] < 0 or line['current'] >= LED_SIZE.w:
             line['done'] = True
             continue
         point = (line['position'], line['current'])
 
         o = line['orientation']
-        adjacent = [((point[0] + i) % W, point[1]) for i in (-1, 1)]
+        adjacent = [(point[0] + i, point[1]) for i in (-1, 1)
+                    if 0 <= point[0] + i < LED_SIZE.w]
         if any(painting[orient(*a, orientation=o)] == BLACK for a in adjacent):
             # we have just intersected another line, maybe end?
-            if random() < 0.25:
+            if random() < PROB_INTERSECT_ENDS:
                 line['done'] = True
 
-        # transform
         point = orient(*point, orientation=o)
-
-        # paint line
         painting[point] = BLACK
 
-    active_lines = [l for l in active_lines if not l['done']]
+    active_lines[:] = [l for l in active_lines if not l['done']]  # modify in-place
+    return init - len(active_lines)
+
+
+def paint_composition(blbox, num_lines, num_paints):
+    painting = {xy: WHITE
+                for xy in product(xrange(LED_SIZE.w), xrange(LED_SIZE.h))}
+    lines = set()
+    done_lines = 0
+    done_paints = 0
+    active_lines = []
+    active_paints = []
+
+    while done_lines < num_lines or done_paints < num_paints:
+        al.draw_pixel_map(blbox, painting)
+        done_lines += grow_lines(painting, active_lines)
+        done_paints += grow_paints(painting, active_paints)
+
+        if len(lines) < num_lines and random() < PROB_LINE**len(lines):
+            add_line(lines, active_lines)
+
+        paints = done_paints + len(active_paints)
+        if (done_lines >= MIN_LINES and paints < num_paints
+                and random() < PROB_COLOR**paints):
+            add_paint(painting, active_paints)
+
+        time.sleep(0.1)
+
+    print 'Done, now enjoy!'
+    time.sleep(DELIGHTMENT_TIME)  # give time to enjoy the resulting pic :)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('--min-lines', default=MIN_LINES, type=int)
+    parser.add_argument('--max-lines', default=MAX_LINES, type=int)
+    parser.add_argument('--min-paints', default=MIN_PAINTS, type=int)
+    parser.add_argument('--max-paints', default=MIN_PAINTS, type=int)
     args = parser.parse_args()
     blbox = al.connect()
-    # random.seed()
-
-    PROB_LINE = 0.5
-    PROB_COLOR = 0.30
+    seed()
 
     while True:
-        al.draw_pixel_map(blbox, painting)
-        grow_lines()
-        grow_paints()
-
-        if random() < PROB_LINE**len(lines):
-            add_line()
-        if len(lines) > 4 and random() < PROB_COLOR**paints:
-            add_paint()
-
-        time.sleep(0.1)
+        num_lines = randint(args.min_lines, args.max_lines)
+        num_paints = randint(args.min_paints, args.max_paints)
+        print "Drawing composition with %d lines and %d paints" % (num_lines, num_paints)
+        paint_composition(blbox, num_lines, num_paints)
